5217accacbdb5ac78d22cd9df90f511960151616
yoshiyahu <anyambuysalem@gmail.com>
2023-09-16 07:20:14 -0700

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,20 @@
 terraform {
 
+/*
   cloud {
-    organization = "organization-name"
+    organization = "josy"
 
     workspaces {
-      name = "learn-terraform-cloud"
+      name = "josy-an-ag"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 4.16"
     }
   }
 
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.2.0"
 }
